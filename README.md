27487c6fd8314bc502993dec24df73bb2a6aa12d
Mustafa Dur <mustafa_dur@hotmail.com>
2021-11-16 13:26:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Appcircle Badge
+
+Add badge to your `.png` files.
+
+Required Input Variables
+- `$AC_ICONS_PATH`: Specifies path of icon files. For example : `$AC_PROJECT_PATH/MyProject/Assets.xcassets/AppIcon.appiconset`
+
+Optional Input Variables
+- `$AC_BADGE_TEXT`: Badge text. `Beta` by default
+- `$AC_BADGE_VERSION`: Version or build number that will appear at the bottom. **TIP**: You can use "\n" in your string for a new line. `1.0` by default
+- `$AC_BADGE_BGCOLOR`: Badge background color. You can use full color name (blue,orange,...), hex codes(#0000FF,#FFA500,...) or rgb values(rgb(0,0,255),rgb(255,165,0),...) `orange` by default
+- `$AC_BADGE_TEXTCOLOR`: Badge version text color. You can use full color name (blue,orange,...), hex codes(#0000FF,#FFA500,...) or rgb values(rgb(0,0,255),rgb(255,165,0),...) `white` by default
+
+Example
+---
+|Original|Modified|
+|--------|------|
+|![Original Image](assets/original.png?raw=true)|![Modified Image](assets/badged.png?raw=true")|
---
+++ main.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -e
+# Modified version of https://github.com/HDB-Li/LLIconVersioning
+
+if [ -z "$AC_ICONS_PATH" ]
+then
+echo "AC_ICONS_PATH is not provided. Skipping step."
+exit 0
+fi
+
+BADGE_TEXT="${AC_BADGE_TEXT:-Beta}"
+BADGE_VERSION="${AC_BADGE_VERSION:-1.0}"
+BADGE_BACKGROUND_COLOR="${AC_BADGE_BGCOLOR:-orange}"
+BADGE_TEXT_COLOR="${AC_BADGE_TEXTCOLOR:-white}"
+
+BADGE_FONT_SIZE=15
+BADGE_HEIGHT=20
+
+# Bottom app information parameters
+ICON_INFO_FONT_SIZE=13
+ICON_INFO_HEIGHT=30
+
+echo "AC_BADGE_TEXT: $BADGE_TEXT"
+echo "AC_BADGE_VERSION: $BADGE_VERSION"
+echo "AC_BADGE_BGCOLOR: $BADGE_BACKGROUND_COLOR"
+echo "AC_BADGE_TEXTCOLOR: $BADGE_TEXT_COLOR"
+
+# Temp images
+AC_TMP_BLURRED="ac_tmp_blurred.png"
+AC_TMP_MASKED="ac_tmp_mask.png"
+AC_TMP_LABELBASE="ac_tmp_labels-base.png"
+AC_TMP_LABELS="ac_tmp_labels.png"
+AC_TMP_TEMP="ac_tmp_temp.png"
+AC_TMP_BADGE="ac_tmp_badge.png"
+
+function processIcon() {
+    base_file=$1
+    BASE_FLODER_PATH=`dirname $base_file`
+    cd "$BASE_FLODER_PATH"
+    width=`identify -format %w ${base_file}`
+    height=`identify -format %h ${base_file}`
+    band_height=$((($height * $ICON_INFO_HEIGHT) / 100))
+    band_position=$(($height - $band_height))
+    text_position=$(($band_position - 3))
+    point_size=$((($ICON_INFO_FONT_SIZE * $width) / 100))
+    badge_width=$((($width * 200) / 100))
+    badge_height=$((($height * $BADGE_HEIGHT) / 100))
+    badge_point_size=$((($BADGE_FONT_SIZE * $width) / 100))
+    target_file=`basename $base_file`
+    convert ${base_file} -blur 10x8 $AC_TMP_BLURRED
+    convert $AC_TMP_BLURRED -gamma 0 -fill white -draw "rectangle 0,$band_position,$width,$height" $AC_TMP_MASKED
+    convert -size ${width}x${band_height} xc:none -fill 'rgba(0,0,0,0.2)' -draw "rectangle 0,0,$width,$band_height" $AC_TMP_LABELBASE
+    convert -background none -size ${width}x${band_height} -pointsize $point_size -fill $BADGE_TEXT_COLOR -gravity center -gravity South caption:"$BADGE_VERSION" $AC_TMP_LABELS
+    convert ${base_file} $AC_TMP_BLURRED $AC_TMP_MASKED -composite $AC_TMP_TEMP
+    convert $AC_TMP_TEMP $AC_TMP_LABELBASE -geometry +0+$band_position -composite $AC_TMP_LABELS -geometry +0+$text_position -geometry +${w}-${h} -composite "${base_file}"
+    convert -background $BADGE_BACKGROUND_COLOR -size ${badge_width}x${badge_height} -pointsize $badge_point_size -fill $BADGE_TEXT_COLOR -gravity center caption:$BADGE_TEXT $AC_TMP_BADGE
+    convert $AC_TMP_BADGE -background none -rotate 45 $AC_TMP_BADGE
+    convert ${base_file} $AC_TMP_BADGE -gravity SouthWest -composite ${base_file}
+
+    if [ $? != 0 ];then
+        echo "convert failed."
+    fi
+
+    rm $AC_TMP_BLURRED
+    rm $AC_TMP_LABELBASE
+    rm $AC_TMP_LABELS
+    rm $AC_TMP_MASKED
+    rm $AC_TMP_TEMP
+    rm $AC_TMP_BADGE
+}
+
+for f in $AC_ICONS_PATH/*.png
+do
+	echo "Adding badge to - $f"
+    processIcon "$f"
+done
