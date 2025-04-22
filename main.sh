37105509737d63c41177004ebda2d6c9df859c3b
ftmzhrsckldr <36725373+ftmzhrsckldr@users.noreply.github.com>
2025-04-22 14:45:25 +0300

    Added badge distance env as optional input var (#1)
    * Added badge distance env as optional input var
    
    * Fixed env name in log
    
    * Fixed percantage logic error
    
    * Updated imagemagick code for Linux os
    
    * Capitalized the first letter words in the title
    
    * Added new if for convert and magick diff command
    
    * Fixed typo and linux LC_ALLL warning
    
    * Removed install comand for imagemagick

--- main.sh
+++ main.sh
@@ -2,36 +2,28 @@
 set -e
 # Modified version of https://github.com/HDB-Li/LLIconVersioning
 
-if [ -z "$AC_ICONS_PATH" ]
-then
-    echo "AC_ICONS_PATH is not provided."
-    exit 1
-fi
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
 AC_ICONS_PATH="$AC_REPOSITORY_DIR/$AC_ICONS_PATH"
-
-if [ ! -d $AC_ICONS_PATH ]
-then
-    echo "Directory $AC_ICONS_PATH DOES NOT exists."
+if compgen -G "$AC_ICONS_PATH" > /dev/null; then
+    echo "Found icon directories matching: $AC_ICONS_PATH"
+else
+    echo "No matching files or directories for: $AC_ICONS_PATH"
     exit 1
 fi
 
-if ! command -v convert &> /dev/null
-then
-    echo "Installing Imagemagick"
-    OS=$(uname -s)
-    if [ "${OS}" == "Linux" ]; then
-        apt-get -y install imagemagick
-        elif [ "${OS}" == "Darwin" ]; then
-        brew install imagemagick
-    fi
+if command -v magick &> /dev/null; then
+    IM_CMD="magick"
 else
-    echo "Imagemagick already installed"
+    IM_CMD="convert"
 fi
 
 BADGE_TEXT="${AC_BADGE_TEXT:-Beta}"
 BADGE_VERSION="${AC_BADGE_VERSION:-1.0}"
 BADGE_BACKGROUND_COLOR="${AC_BADGE_BGCOLOR:-orange}"
 BADGE_TEXT_COLOR="${AC_BADGE_TEXTCOLOR:-white}"
+BADGE_CORNER_SHIFT=$(( ${AC_BADGE_CORNER_SHIFT:-5} ))
 
 BADGE_FONT_SIZE=15
 BADGE_HEIGHT=20
@@ -44,6 +36,7 @@ echo "AC_BADGE_TEXT: $BADGE_TEXT"
 echo "AC_BADGE_VERSION: $BADGE_VERSION"
 echo "AC_BADGE_BGCOLOR: $BADGE_BACKGROUND_COLOR"
 echo "AC_BADGE_TEXTCOLOR: $BADGE_TEXT_COLOR"
+echo "AC_BADGE_CORNER_SHIFT: $BADGE_CORNER_SHIFT"
 
 # Temp images
 AC_TMP_BLURRED="ac_tmp_blurred.png"
@@ -52,6 +45,7 @@ AC_TMP_LABELBASE="ac_tmp_labels-base.png"
 AC_TMP_LABELS="ac_tmp_labels.png"
 AC_TMP_TEMP="ac_tmp_temp.png"
 AC_TMP_BADGE="ac_tmp_badge.png"
+AC_TMP_BADGE_BG="ac_tmp_badge_bg.png"
 
 function processIcon() {
     base_file=$1
@@ -59,26 +53,36 @@ function processIcon() {
     cd "$BASE_FLODER_PATH"
     width=`identify -format %w ${base_file}`
     height=`identify -format %h ${base_file}`
+    badge_width_offset=$(( ($width * $BADGE_CORNER_SHIFT) / 100 ))
+    badge_height_offset=$(( ($height * $BADGE_CORNER_SHIFT) / 100 ))
     band_height=$((($height * $ICON_INFO_HEIGHT) / 100))
     band_position=$(($height - $band_height))
-    text_position=$(($band_position - 3))
+    text_offset=$(awk "BEGIN {print int(1/5 * $band_position + 0.5)}")
+    text_position=$(($band_position - $text_offset))
     point_size=$((($ICON_INFO_FONT_SIZE * $width) / 100))
     badge_width=$((($width * 200) / 100))
     badge_height=$((($height * $BADGE_HEIGHT) / 100))
     badge_point_size=$((($BADGE_FONT_SIZE * $width) / 100))
-    target_file=`basename $base_file`
-    convert ${base_file} -blur 10x8 $AC_TMP_BLURRED
-    convert $AC_TMP_BLURRED -gamma 0 -fill white -draw "rectangle 0,$band_position,$width,$height" $AC_TMP_MASKED
-    convert -size ${width}x${band_height} xc:none -fill 'rgba(0,0,0,0.2)' -draw "rectangle 0,0,$width,$band_height" $AC_TMP_LABELBASE
-    convert -background none -size ${width}x${band_height} -pointsize $point_size -fill $BADGE_TEXT_COLOR -gravity center -gravity South caption:"$BADGE_VERSION" $AC_TMP_LABELS
-    convert ${base_file} $AC_TMP_BLURRED $AC_TMP_MASKED -composite $AC_TMP_TEMP
-    convert $AC_TMP_TEMP $AC_TMP_LABELBASE -geometry +0+$band_position -composite $AC_TMP_LABELS -geometry +0+$text_position -geometry +${w}-${h} -composite "${base_file}"
-    convert -background $BADGE_BACKGROUND_COLOR -size ${badge_width}x${badge_height} -pointsize $badge_point_size -fill $BADGE_TEXT_COLOR -gravity center caption:$BADGE_TEXT $AC_TMP_BADGE
-    convert $AC_TMP_BADGE -background none -rotate 45 $AC_TMP_BADGE
-    convert ${base_file} $AC_TMP_BADGE -gravity SouthWest -composite ${base_file}
-    
+
+    $IM_CMD ${base_file} -blur 10x8 $AC_TMP_BLURRED
+    if [[ "$IM_CMD" == "magick" ]]; then
+        $IM_CMD -size ${width}x${height} xc:black -fill white -draw "rectangle 0,$band_position $width,$height" $AC_TMP_MASKED
+        $IM_CMD $AC_TMP_BLURRED $AC_TMP_MASKED -alpha off -compose CopyOpacity -composite $AC_TMP_MASKED
+        $IM_CMD "${base_file}" $AC_TMP_MASKED -compose over -composite $AC_TMP_TEMP
+    else
+        $IM_CMD $AC_TMP_BLURRED -gamma 0 -fill white -draw "rectangle 0,$band_position,$width,$height" $AC_TMP_MASKED
+    fi
+    $IM_CMD -size ${width}x${band_height} xc:none -fill 'rgba(0,0,0,0.2)' -draw "rectangle 0,0,$width,$band_height" $AC_TMP_LABELBASE
+    $IM_CMD -background none -size ${width}x${band_height} -pointsize $point_size -fill $BADGE_TEXT_COLOR -gravity center -gravity South caption:"$BADGE_VERSION" $AC_TMP_LABELS
+    $IM_CMD ${base_file} $AC_TMP_BLURRED $AC_TMP_MASKED -composite $AC_TMP_TEMP
+    $IM_CMD $AC_TMP_TEMP $AC_TMP_LABELBASE -geometry +0+$band_position -composite $AC_TMP_LABELS -geometry +0+$text_position -geometry +${w}-${h} -composite "${base_file}"
+    $IM_CMD -size ${badge_width}x${badge_height} xc:$BADGE_BACKGROUND_COLOR $AC_TMP_BADGE_BG
+    $IM_CMD $AC_TMP_BADGE_BG -gravity center -fill $BADGE_TEXT_COLOR -pointsize $badge_point_size -annotate +0+0 "$BADGE_TEXT" $AC_TMP_BADGE
+    $IM_CMD $AC_TMP_BADGE -background none -rotate 45 $AC_TMP_BADGE
+    $IM_CMD $base_file $AC_TMP_BADGE -gravity SouthWest -geometry -${badge_width_offset}-${badge_height_offset} -composite $base_file
+
     if [ $? != 0 ];then
-        echo "convert failed."
+        echo "The imagemagick command failed."
     fi
     
     rm $AC_TMP_BLURRED
@@ -87,6 +91,7 @@ function processIcon() {
     rm $AC_TMP_MASKED
     rm $AC_TMP_TEMP
     rm $AC_TMP_BADGE
+    rm $AC_TMP_BADGE_BG
 }
 
 for f in  `find  $AC_ICONS_PATH -name '*.png'`
